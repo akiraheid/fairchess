d7dc84510205e5d08c37975efe83c5c060b4b3b0
akira <akiraheid@users.noreply.github.com>
2019-02-23 20:08:42 -0500

    Convert to Bootstrap
    Turn display changed from text to color for both mobile and desktop
    views.
    
    Closes #16

--- client/js/gameLogic.js
+++ client/js/gameLogic.js
@@ -2,25 +2,11 @@ var board;
 var socket = io();
 var game = new Chess();
 var messageField = $('#message');
-var messageFields = [];
+var status2Field = $('#status-2');
 var turns = ['w','b','b','w','b','w','w','b','b','w','w','b','w'];
 var turnCount = 0;
 var kingTaken = false;
 var gameOver = false;
-
-messageFields[0] = $('#message0');
-messageFields[1] = $('#message1');
-messageFields[2] = $('#message2');
-messageFields[3] = $('#message3');
-messageFields[4] = $('#message4');
-messageFields[5] = $('#message5');
-messageFields[6] = $('#message6');
-messageFields[7] = $('#message7');
-messageFields[8] = $('#message8');
-messageFields[9] = $('#message9');
-messageFields[10] = $('#message10');
-messageFields[11] = $('#message11');
-messageFields[12] = $('#message12');
 
 // Only move if it is their turn and game is not over.
 // Modified chessboardjs.com/examples#5000
@@ -65,12 +51,10 @@ function updateStatus(winner, method) {
 	var messageNumb = turnCount.toString();
 	var message = '';
 	var moveColor = 'White';
-	var messages = [];
 	var moveColors = [];
 
-	for (i = 0; i <= 12; i++) {
+	for (i = 0; i < turns.length; i++) {
 		turns[i] = ((bitSum(turnCount + i) === 0) ? 'w' : 'b');
-		messages[i] = '';
 		moveColors[i] = ((turns[i] === 'b') ? 'Black' : 'White');
 	}
 
@@ -88,20 +72,25 @@ function updateStatus(winner, method) {
 			kingTaken = true;
 			message = 'Game over: ' + moveColors[0] + ' king taken';
 		}
+		messageField.toggleClass('bg-danger');
+		status2Field.toggleClass('bg-danger');
 	} else {
-		for (i = 0; i <= 12; i++) {
-			messages[i] = moveColors[i] + ' to move';
-		}
-
 		if (game.in_check() === true) {
-			message = moveColors[0] + ' is in check';
+			message = moveColors[0] + ' in check';
+			messageField.toggleClass('bg-warning text-dark');
+			status2Field.toggleClass('bg-warning text-dark');
+		} else {
+			messageField.toggleClass('bg-danger bg-warning text-dark', false);
+			status2Field.toggleClass('bg-danger bg-warning text-dark', false);
 		}
 	}
 
 	messageField.html(message);
+	status2Field.html(message);
 
-	for (i = 0; i <= 12; i++) {
-		messageFields[i].html(messages[i]);
+	for (i = 0; i < turns.length; i++) {
+		$('#turn-' + i).css('background-color', moveColors[i].toLowerCase());
+		$('#message-' + i).css('background-color', moveColors[i].toLowerCase());
 	}
 };
 
@@ -135,6 +124,14 @@ socket.on('other-move', data => {
 	updateMove(data);
 });
 
+function resize() {
+	board.resize();
+	let width = $('#board').css('width');
+	$("div[class^='board-']").css('position', 'static');
+	// Remove weird chessboardjs left offset
+	$("div[id^='board-']").css('left', '');
+}
+
 socket.on('multiplayer-found', data => {
 	console.log('found another player ' + data.opponentID);
 
@@ -150,12 +147,15 @@ socket.on('multiplayer-found', data => {
 	};
 	board = ChessBoard('board', boardConfig);
 	updateStatus(null, null);
-	$('#game').css('display', 'inline');
+	$('#game').css('display', '');
 	$('#waiting').css('display', 'none');
+	resize();
+	$(window).resize(resize);
 });
 
 var init = function() {
 	socket.emit('multiplayer-searching', {});
+	$('#game').css('display', 'none');
 };
 
 $(document).ready(init);
